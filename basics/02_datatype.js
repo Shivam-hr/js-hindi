9fbfa9247b8c53070f6d9337ea845cb4149e0ee9
Shivam-hr <shivamhrsalwan@gmail.com>
2025-08-28 15:17:12 +0000

    Datatypes

---
+++ basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version 
+
+// alert("Hello") // there should be error bcoz we are using node js not browser
+
+// alert(3+3) // there should be error bcoz we are using node js not browser
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true / false
+// null=> standalone value ("it is empty value") this is a special type
+// undefined => value not defined
+// symbol => unique
+
+
+// #objects
+
+console.log(typeof 12);
+console.log(typeof null); // object
+console.log(typeof undefined); //number
+
+
+
+
+//for documantation use MDN orignal is ECMA but MDN is better so go for that

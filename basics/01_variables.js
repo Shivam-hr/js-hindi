0943d44bd351c624e9d7e2f259f60d66718fab02
Shivam-hr <shivamhrsalwan@gmail.com>
2025-08-31 11:32:37 +0000

    Conversion of datatype

---
+++ basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+
+let accountEmail = "Shivamhr@gmail.com"
+
+var accountPassword = "123456"
+
+accountCity = "Jaipur"
+
+//accountId = 2321 // not allowed
+
+accountEmail = "Shivam@@"
+accountPassword = "212121"
+accountCity = "Benguluru"
+let accountState;
+
+console.table([accountId, accountEmail , accountPassword , accountCity ,accountState])
+
+
+/*
+Prefer not to use var
+bcoz of issue in block scope and functional scope
+
+*/
+
+
---
+++ basics/03_conversion.js
@@ -0,0 +1,51 @@
+let score = "33abbc"
+let score1 = 33
+let score2 = null
+let score3 = undefined
+
+
+
+//console.log(typeof score);
+//console.log(typeof score1);
+
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);  number
+
+//console.log(valueInNumber); 33
+
+//let valueInNumber = Number(score2) number
+//console.log(typeof valueInNumber); 
+
+//console.log(valueInNumber); 0
+
+//let valueInNumber = Number(score3) 
+//console.log(typeof valueInNumber); // number
+
+//console.log(valueInNumber); // nan
+
+
+// "33 == 33"
+// "33abc = NaN not a number"
+// true = 1; false =0
+
+
+//let isLoggedIn = ""
+
+//let booleanIsLoogedIn = 1
+
+//let booleanIsLoogedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoogedIn)
+
+// 1 => true , 0 => false
+
+// "" => flase
+
+// "anything" => true
+
+let someNumber = 33.25
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
